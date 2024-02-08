10357d321ce3599f8ab1a2c106f045a940daa324
Andhika-GIT <andhikapr081@gmail.com>
2024-02-08 21:36:49 +0700

    feat : GORM struct modal intialization

--- model/domain/event.go
+++ model/domain/event.go
@@ -3,11 +3,16 @@ package domain
 import "time"
 
 type Event struct {
-	Id          int32
-	User_id     int32
-	Name        string
-	Description string
-	Start_time  time.Time
-	Created_at  time.Time
-	Updated_at  time.Time
+	Id          int32     `gorm:"primary_key;column:id"`
+	User_id     int32     `gorm:"column:user_id"`
+	Name        string    `gorm:"column:name"`
+	Description string    `gorm:"column:description"`
+	Start_time  time.Time `gorm:"column:start_time"`
+	End_time    time.Time `gorm:"column:end_time"`
+	Created_at  time.Time `gorm:"column:created_at"`
+	Updated_at  time.Time `gorm:"column:updated_at"`
+}
+
+func (e *Event) TableName() string {
+	return "events"
 }
--- model/domain/user.go
+++ model/domain/user.go
@@ -3,10 +3,14 @@ package domain
 import "time"
 
 type User struct {
-	Id         int32
-	Name       string
-	Email      string
-	Password   string
-	Created_at time.Time
-	Updated_at time.Time
+	Id         int32     `gorm:"primary_key;column:id"`
+	Name       string    `gorm:"column:name"`
+	Email      string    `gorm:"column:email"`
+	Password   string    `gorm:"column:password "`
+	Created_at time.Time `gorm:"column:created_at"`
+	Updated_at time.Time `gorm:"column:updated_at"`
+}
+
+func (u *User) TableName() string {
+	return "users"
 }
