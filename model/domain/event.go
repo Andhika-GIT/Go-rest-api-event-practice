9178bd0ff6fde341bcb54f3c869758d1036dfccd
Andhika-GIT <andhikapr081@gmail.com>
2024-02-09 19:33:12 +0700

    feat : apply field permission feature

--- model/domain/event.go
+++ model/domain/event.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type Event struct {
-	Id          int32     `gorm:"primary_key;column:id"`
+	Id          int32     `gorm:"primary_key;column:id;<-:create"`
 	User_id     int32     `gorm:"column:user_id"`
 	Name        string    `gorm:"column:name"`
 	Description string    `gorm:"column:description"`
 	Start_time  time.Time `gorm:"column:start_time"`
 	End_time    time.Time `gorm:"column:end_time"`
-	Created_at  time.Time `gorm:"column:created_at;autoCreateTime"`
+	Created_at  time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
 	Updated_at  time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
 }
 
