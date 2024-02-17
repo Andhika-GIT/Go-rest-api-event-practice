3d9c583ffec9c527d900755836e7ae43c20cdef1
Andhika Prasetya <andhikapr081@gmail.com>
2024-02-17 20:28:09 +0700

    feat : completed user repository

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -11,6 +11,5 @@ type UserRepository interface {
 	Save(ctx context.Context, tx *gorm.DB, user domain.User) (domain.User, error)
 	Update(ctx context.Context, tx *gorm.DB, user domain.User) (domain.User, error)
 	Delete(ctx context.Context, tx *gorm.DB, user domain.User) (domain.User, error)
-	Find(ctx context.Context, tx *gorm.DB, user domain.User) (domain.User, error)
 	FindById(ctx context.Context, tx *gorm.DB, userId int32) (domain.User, error)
 }
--- repository/user_repository_impl.go
+++ repository/user_repository_impl.go
@@ -22,3 +22,36 @@ func (repository *UserRepositoryImpl) Save(ctx context.Context, tx *gorm.DB, use
 
 	return user, nil
 }
+
+func (repository *UserRepositoryImpl) Update(ctx context.Context, tx *gorm.DB, user domain.User) (domain.User, error) {
+	err := tx.Save(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (repository *UserRepositoryImpl) Delete(ctx context.Context, tx *gorm.DB, user domain.User) (domain.User, error) {
+	err := tx.Where("id = ?", user.Id).Delete(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (repository *UserRepositoryImpl) FindById(ctx context.Context, tx *gorm.DB, userId int32) (domain.User, error) {
+
+	user := domain.User{}
+	err := tx.First(&user, userId).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+
+}
